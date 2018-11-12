cca49258f1444f0807d7e5bd770ad30a9e91c785
masato25 <leomasato25@gmail.com>
2018-11-12 16:38:35 +0800

    config truffle.js template

--- truffle.js
+++ truffle.js
@@ -1,18 +1,34 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+var PrivateKeyProvider = require("truffle-privatekey-provider");
+var privateKey = "";
+var infuraAPIKey = "";
+var improvider = new PrivateKeyProvider(privateKey, `https://ropsten.infura.io/${infuraAPIKey}`);
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545, // ganache-cli
+      network_id: '*', // Match any network id
+      from: "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d",
+      gas: 6700000,
+      gasPrice: 0x01
+    },
+    ropsten: {
+      provider: improvider,
+      network_id: 3,
+      gas: 6000000,
+      gasPrice: 40000000000,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.4.25",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
