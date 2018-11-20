ee34648591fca30c8cde44b944d38886522adeec
Masato25 <leomasato25@gmail.com>
2018-11-20 14:52:27 +0800

    [test] extended mock variables.

--- test/mock/variable.js
+++ test/mock/variable.js
@@ -1,7 +1,11 @@
-var _name = _symbol = _currency = "1SGD";
-var _decimals = 18;
-var _admin = _masterMinter = _pauser = _blacklister = "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d";
-var _owner = "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d";
+const _name = _symbol = _currency = "1SGD";
+const _decimals = 18;
+const _admin = "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d"; //account[4]
+const _masterMinter = "0xd03ea8624C8C5987235048901fB614fDcA89b117"; //account[5]
+const _pauser = "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d"; //account[4]
+const _blacklister = "0x95cED938F7991cd0dFcb48F0a06a40FA1aF46EBC"; //account[6]
+const _owner = "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d"; //account[4]
+const _other_account = "0x3E5e9111Ae8eB78Fe1CC3bb8915d5D461F3Ef9A9"; //account[7]
 
 module.exports = {
   _name,
@@ -9,8 +13,10 @@ module.exports = {
   _currency,
   _decimals,
   _admin,
+  ZERO_ADDRESS: "0x0000000000000000000000000000000000000000",
   _masterMinter,
   _pauser,
   _blacklister,
-  _owner
+  _owner,
+  _other_account
 }
