27aecf668cb01e7aeb31c1b38de98c866f4ac2e8
Masato25 <leomasato25@gmail.com>
2018-11-20 14:48:41 +0800

    [test] add first test case

---
+++ test/mock/variable.js
@@ -0,0 +1,16 @@
+var _name = _symbol = _currency = "1SGD";
+var _decimals = 18;
+var _admin = _masterMinter = _pauser = _blacklister = "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d";
+var _owner = "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d";
+
+module.exports = {
+  _name,
+  _symbol,
+  _currency,
+  _decimals,
+  _admin,
+  _masterMinter,
+  _pauser,
+  _blacklister,
+  _owner
+}
